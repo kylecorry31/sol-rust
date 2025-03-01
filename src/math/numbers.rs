cc811491709b886ee9282df8d4f5c25fbeba7313
Kyle Corry <kylecorry31@gmail.com>
2025-03-01 10:05:18 -0500

    Add more math utils

--- src/math/numbers.rs
+++ src/math/numbers.rs
@@ -8,16 +8,34 @@ pub trait Number:
     + PartialEq
     + PartialOrd
     + Copy
+    + Default
 {
+    fn from_i32(value: i32) -> Self;
 }
 
 pub trait Integer: Number {}
 pub trait Real: Number {}
 
-impl Number for f32 {}
-impl Number for f64 {}
-impl Number for i32 {}
-impl Number for i64 {}
+impl Number for f32 {
+    fn from_i32(value: i32) -> Self {
+        value as f32
+    }
+}
+impl Number for f64 {
+    fn from_i32(value: i32) -> Self {
+        value as f64
+    }
+}
+impl Number for i32 {
+    fn from_i32(value: i32) -> Self {
+        value
+    }
+}
+impl Number for i64 {
+    fn from_i32(value: i32) -> Self {
+        value as i64
+    }
+}
 
 impl Integer for i32 {}
 impl Integer for i64 {}
--- src/math/utils.rs
+++ src/math/utils.rs
@@ -1,6 +1,6 @@
 use super::numbers::Number;
 
-pub fn constrain<T: PartialOrd>(value: T, min: T, max: T) -> T {
+pub fn clamp<T: PartialOrd>(value: T, min: T, max: T) -> T {
     if value > max {
         return max;
     }
@@ -21,35 +21,156 @@ pub fn wrap<T: Number>(value: T, min: T, max: T) -> T {
     value
 }
 
+pub fn lerp<T: Number>(percent: T, start: T, end: T, should_clamp: bool) -> T {
+    let value = start + (end - start) * percent;
+
+    if should_clamp {
+        clamp(value, start, end)
+    } else {
+        value
+    }
+}
+
+pub fn norm<T: Number>(value: T, minimum: T, maximum: T, should_clamp: bool) -> T {
+    let range = maximum - minimum;
+    if range == T::from_i32(0) {
+        return T::from_i32(0);
+    }
+    let normal = (value - minimum) / range;
+
+    if should_clamp {
+        clamp(normal, T::from_i32(0), T::from_i32(1))
+    } else {
+        normal
+    }
+}
+
+pub fn map<T: Number>(
+    value: T,
+    original_min: T,
+    original_max: T,
+    new_min: T,
+    new_max: T,
+    should_clamp: bool,
+) -> T {
+    let normal = norm(value, original_min, original_max, should_clamp);
+    lerp(normal, new_min, new_max, should_clamp)
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
     use rstest::rstest;
 
     #[rstest]
-    #[case(5.0, 0.0, 10.0, 5.0)]
-    #[case(15.0, 0.0, 10.0, 10.0)]
-    #[case(-5.0, 0.0, 10.0, 0.0)]
-    #[case(10.0, 0.0, 10.0, 10.0)]
-    #[case(0.0, 0.0, 10.0, 0.0)]
-    #[case(5.0, 5.0, 5.0, 5.0)]
-    fn test_constrain(
-        #[case] value: f32,
-        #[case] min: f32,
-        #[case] max: f32,
-        #[case] expected: f32,
-    ) {
-        assert_eq!(constrain(value, min, max), expected);
+    #[case(0.1, 0.0, 1.0, 0.1)]
+    #[case(0.0, 0.0, 1.0, 0.0)]
+    #[case(1.0, 0.0, 1.0, 1.0)]
+    #[case(1.2, 0.0, 1.0, 1.0)]
+    #[case(-0.1, 0.0, 1.0, 0.0)]
+    #[case(4.0, 2.0, 5.0, 4.0)]
+    #[case(1.0, 2.0, 5.0, 2.0)]
+    #[case(6.0, 2.0, 5.0, 5.0)]
+    fn test_clamp(#[case] value: f32, #[case] min: f32, #[case] max: f32, #[case] expected: f32) {
+        assert_eq!(clamp(value, min, max), expected);
     }
 
     #[rstest]
-    #[case(5.0, 0.0, 10.0, 5.0)]
-    #[case(15.0, 0.0, 10.0, 5.0)]
-    #[case(-5.0, 0.0, 10.0, 5.0)]
-    #[case(10.0, 0.0, 10.0, 10.0)]
-    #[case(0.0, 0.0, 10.0, 0.0)]
-    #[case(5.0, 5.0, 5.0, 5.0)]
+    #[case(0.2, 0.0, 1.0, 0.2)]
+    #[case(1.0, 0.0, 1.0, 1.0)]
+    #[case(0.0, 0.0, 1.0, 0.0)]
+    #[case(1.5, 0.0, 1.0, 0.5)]
+    #[case(-0.75, 0.0, 1.0, 0.25)]
+    #[case(0.0, 1.0, 4.0, 3.0)]
+    #[case(5.0, 1.0, 4.0, 2.0)]
+    #[case(6.0, 5.0, 4.0, 5.0)]
+    #[case(0.0, 0.0, 0.0, 0.0)]
+    #[case(1800.0, 0.0, 360.0, 0.0)]
+    #[case(-1800.0, 0.0, 360.0, 360.0)]
+    #[case(1799.0, 0.0, 360.0, 359.0)]
+    #[case(-1799.0, 0.0, 360.0, 1.0)]
     fn test_wrap(#[case] value: f32, #[case] min: f32, #[case] max: f32, #[case] expected: f32) {
         assert_eq!(wrap(value, min, max), expected);
+    }
+
+    #[rstest]
+    #[case(0.1, 0.0, 1.0, false, 0.1)]
+    #[case(0.0, 0.0, 1.0, false, 0.0)]
+    #[case(1.0, 0.0, 1.0, false, 1.0)]
+    #[case(1.2, 0.0, 1.0, false, 1.2)]
+    #[case(-0.1, 0.0, 1.0, false, -0.1)]
+    #[case(4.0, 2.0, 6.0, false, 0.5)]
+    #[case(1.0, 2.0, 6.0, false, -0.25)]
+    #[case(6.0, 2.0, 6.0, false, 1.0)]
+    #[case(2.0, 2.0, 6.0, false, 0.0)]
+    #[case(-1.0, 0.0, 1.0, true, 0.0)]
+    #[case(0.5, 0.0, 1.0, true, 0.5)]
+    #[case(2.0, 0.0, 1.0, true, 1.0)]
+    fn test_norm(
+        #[case] value: f32,
+        #[case] min: f32,
+        #[case] max: f32,
+        #[case] should_clamp: bool,
+        #[case] expected: f32,
+    ) {
+        assert_eq!(norm(value, min, max, should_clamp), expected);
+    }
+
+    #[rstest]
+    #[case(0.1, 0.0, 1.0, false, 0.1)]
+    #[case(0.0, 0.0, 1.0, false, 0.0)]
+    #[case(1.0, 0.0, 1.0, false, 1.0)]
+    #[case(1.2, 0.0, 1.0, false, 1.2)]
+    #[case(-0.1, 0.0, 1.0, false, -0.1)]
+    #[case(0.5, 2.0, 6.0, false, 4.0)]
+    #[case(-0.25, 2.0, 6.0, false, 1.0)]
+    #[case(1.0, 2.0, 6.0, false, 6.0)]
+    #[case(0.0, 2.0, 6.0, false, 2.0)]
+    #[case(-1.0, 0.0, 1.0, true, 0.0)]
+    #[case(0.5, 0.0, 1.0, true, 0.5)]
+    #[case(2.0, 0.0, 1.0, true, 1.0)]
+    fn test_lerp(
+        #[case] percent: f32,
+        #[case] start: f32,
+        #[case] end: f32,
+        #[case] should_clamp: bool,
+        #[case] expected: f32,
+    ) {
+        assert_eq!(lerp(percent, start, end, should_clamp), expected);
+    }
+
+    #[rstest]
+    #[case(0.1, 0.0, 1.0, 2.0, 4.0, false, 2.2)]
+    #[case(0.0, 0.0, 1.0, 2.0, 4.0, false, 2.0)]
+    #[case(1.0, 0.0, 1.0, 2.0, 4.0, false, 4.0)]
+    #[case(1.2, 0.0, 1.0, 2.0, 4.0, false, 4.4)]
+    #[case(-0.1, 0.0, 1.0, 2.0, 4.0, false, 1.8)]
+    #[case(4.0, 2.0, 6.0, 0.0, 4.0, false, 2.0)]
+    #[case(1.0, 2.0, 6.0, 0.0, 4.0, false, -1.0)]
+    #[case(6.0, 2.0, 6.0, 0.0, 4.0, false, 4.0)]
+    #[case(2.0, 2.0, 6.0, 0.0, 4.0, false, 0.0)]
+    #[case(-1.0, 0.0, 1.0, 2.0, 4.0, true, 2.0)]
+    #[case(0.5, 0.0, 1.0, 2.0, 4.0, true, 3.0)]
+    #[case(2.0, 0.0, 1.0, 2.0, 4.0, true, 4.0)]
+    fn test_map(
+        #[case] value: f32,
+        #[case] original_min: f32,
+        #[case] original_max: f32,
+        #[case] new_min: f32,
+        #[case] new_max: f32,
+        #[case] should_clamp: bool,
+        #[case] expected: f32,
+    ) {
+        assert_eq!(
+            map(
+                value,
+                original_min,
+                original_max,
+                new_min,
+                new_max,
+                should_clamp
+            ),
+            expected
+        );
     }
 }
