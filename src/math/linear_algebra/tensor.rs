ef18699b753f60f3cd356d5247bb733ca58048d2
Kyle Corry <kylecorry31@gmail.com>
2025-03-02 12:42:14 -0500

    Expose linalg methods on trait

--- src/math/linear_algebra/tensor.rs
+++ src/math/linear_algebra/tensor.rs
@@ -1,4 +1,4 @@
-use super::{Matrix, RawTensor, add, cross, dot, subtract};
+use super::{Matrix, RawTensor, add, cross, divide, dot, multiply, scale, subtract, transpose};
 
 pub trait Tensor {
     fn get_tensor(&self) -> &RawTensor;
@@ -11,8 +11,20 @@ pub trait Tensor {
     fn add<T: Tensor>(&self, other: &T) -> Matrix {
         Matrix::new(add(self.get_tensor(), other.get_tensor()))
     }
-    fn sub<T: Tensor>(&self, other: &T) -> Matrix {
+    fn transpose(&self) -> Matrix {
+        Matrix::new(transpose(self.get_tensor()))
+    }
+    fn subtract<T: Tensor>(&self, other: &T) -> Matrix {
         Matrix::new(subtract(self.get_tensor(), other.get_tensor()))
+    }
+    fn multiply<T: Tensor>(&self, other: &T) -> Matrix {
+        Matrix::new(multiply(self.get_tensor(), other.get_tensor()))
+    }
+    fn divide<T: Tensor>(&self, other: &T) -> Matrix {
+        Matrix::new(divide(self.get_tensor(), other.get_tensor()))
+    }
+    fn scale(&self, factor: f32) -> Matrix {
+        Matrix::new(scale(self.get_tensor(), factor))
     }
     fn value(&self) -> f32 {
         self.get_tensor()[0][0]
